fa42cf1d34c753b9e6dc6cd8aa287a10cf503248
ailapina150 <alapina840@gmail.com>
2024-12-21 18:59:48 +0300

    my-chart-bot

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        DefaultBotOptions botOptions = new DefaultBotOptions();
+        botOptions.setProxyHost("74.56.228.180");
+        botOptions.setProxyPort(4145);
+        botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS4);
+
+        MyBot myBot = new MyBot();
+        try {
+            telegramBotsApi.registerBot(myBot);
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/MyBot.java
@@ -0,0 +1,49 @@
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class MyBot extends TelegramLongPollingBot {
+    public static final String USER_NAME = "chat_21122024_bot";
+    public static final String TOKEN = "8147125332:AAG1gOR5wjPGGozT2hFVg1Ow1KZEhdFKXCA";
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()){
+            Long chartId = update.getMessage().getChatId();
+            String message = update.getMessage().getText();
+            switch (message) {
+                case"/start": sendMassage(chartId, "Привет, "+ update.getMessage().getChat().getFirstName()
+                        + "! Купи слона.");
+                    break;
+                default: sendMassage(chartId, "Все говорят: \"" + message + "\" А ты возьми и купи слона.");
+            }
+
+        }
+
+    }
+     private void sendMassage(Long chartId, String text){
+         SendMessage reply = new SendMessage();
+         reply.setChatId(chartId.toString());
+         reply.setText(text);
+         try {
+             execute(reply);
+         } catch (TelegramApiException e) {
+             e.printStackTrace();
+         }
+     }
+
+    @Override
+    public String getBotUsername() {
+        return USER_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return TOKEN;
+    }
+
+}
