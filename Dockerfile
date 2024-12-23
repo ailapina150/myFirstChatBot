9f79de0985e5daec25d04e39b525878790c4cefd
ailapina150 <alapina840@gmail.com>
2024-12-23 13:58:04 +0300

    ready for deploy

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM maven:3.6-openjdk-16-slim AS build
-COPY pom.xml .
-WORKDIR src ./src
-RUN mvn clean package
+COPY src ./src
+COPY target ./target
+COPY pom.xml ./pom.xml
 
-FROM openjdk:16-jre-slim
+FROM openjdk:16.0-slim
 COPY --from=build ./target/*.jar /app.jar
 CMD ["java", "-jar", "/app.jar"]
+
--- src/main/java/MyBot.java
+++ src/main/java/MyBot.java
@@ -1,7 +1,5 @@
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
